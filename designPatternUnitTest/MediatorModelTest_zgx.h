73b35ce79f683f7530f5c4f3409245e7f59c18df
hanqishao <49721268+hanqishao@users.noreply.github.com>
2020-11-20 18:37:08 +0800

    添加了ZGX的测试代码 (#20)
    * ZGX的测试代码
    
    * 修改测试项目使其符合更新后的代码
    
    * 添加各测试代码到项目中
    
    Co-authored-by: QingXuDw <49812095+QingXuDw@users.noreply.github.com>

---
+++ designPatternUnitTest/MediatorModelTest_zgx.h
@@ -0,0 +1,83 @@
+﻿#pragma once
+#include "pch.h"
+#include "../designPattern/ShoppingMall.h"
+#include"../designPattern/MediatorModel.h"
+#include <vector>
+
+//用namespace包裹以防止命名冲突
+namespace zgx {
+	/************************************************************************************
+	*																					*
+	*								GoodsTest货物测试用例									*
+	*																					*
+	*					负责测试ShoppingMall.h中的Abstract_goods类及其子类					*
+	*************************************************************************************/
+	/*class ColleagueTest : public RedirectIO {
+	protected:
+		//用于维护各子类对象的vector
+		vector<Colleague*> Colleagues;
+		//在Colleagues中为每一个子类生成一个对象
+		void SetUp() override {
+			RedirectIO::SetUp();
+			Colleague* p_colleague;
+			Mediator* m_mediator;
+			p_colleague = new ConcreteColleague_0(m_mediator);
+			Colleagues.push_back(p_colleague);
+			p_colleague = new ConcreteColleague_1(m_mediator);
+			Colleagues.push_back(p_colleague);
+		}
+		//清理Colleagues中的全部子类对象
+		void TearDown() override {
+			RedirectIO::TearDown();
+			for (std::vector<Colleague*>::iterator i = Colleagues.begin();
+				i != Colleagues.end();
+				i++)
+			{
+				S_DELETE((*i));
+			}
+		}
+	};
+
+	TEST_F(ColleagueTest, send) {
+		for (std::vector<Colleague*>::iterator i = Colleagues.begin();
+			i != Colleagues.end();
+			i++)
+		{
+			EXPECT_NO_THROW((*i)->send(""));
+			EXPECT_NO_THROW((*i)->send("10086"));
+		}
+	}
+
+	TEST_F(ColleagueTest, notify) {
+		for (std::vector<Colleague*>::iterator i = Colleagues.begin();
+			i != Colleagues.end();
+			i++)
+		{
+			EXPECT_NO_THROW((*i)->notify(""));
+			EXPECT_NO_THROW((*i)->notify("10086"));
+		}
+	}
+	*/
+	class MediatorManagerTest : public RedirectIO {
+	protected:
+		MediatorManager* p_mediatorManager;
+		void SetUp() override {
+			RedirectIO::SetUp();
+			p_mediatorManager = &(MediatorManager::getInstance());
+		}
+		void TearDown() override
+		{
+			RedirectIO::TearDown();
+		}
+	};
+	TEST_F(MediatorManagerTest, HandleCommand) {
+		EXPECT_NO_THROW(p_mediatorManager->handleCommand(""));
+		EXPECT_NO_THROW(p_mediatorManager->handleCommand("mediator"));
+		EXPECT_NO_THROW(p_mediatorManager->handleCommand("mediator make"));
+		EXPECT_NO_THROW(p_mediatorManager->handleCommand("mediator sendmessage"));
+		EXPECT_NO_THROW(p_mediatorManager->handleCommand("JLNB!!!"));
+	}
+
+
+}
+
