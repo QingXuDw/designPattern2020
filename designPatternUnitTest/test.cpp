629ccc4d31316740e0c1250dc378a2ddd8293d53
HoldTheDoors <45756786+HoldTheDoors@users.noreply.github.com>
2020-11-28 15:38:33 +0800

    添加了wsq的测试文件 (#24)
    * 添加了wsq的测试文件
    
    * 修改工具集和目标SDK与大家一致
    
    * Update designPattern.vcxproj
    
    * 修改错误的编码格式
    
    * 规范化格式
    
    Co-authored-by: QingXuDw <49812095+QingXuDw@users.noreply.github.com>

--- designPatternUnitTest/test.cpp
+++ designPatternUnitTest/test.cpp
@@ -9,12 +9,14 @@
 #include "SnowmanTest_zgx.h"
 #include "TestPlanTest_zgx.h"
 #include "TouristCenterTest_zgx.h"
+#include "VisitorClassesTest_wsq.h"
+#include "StateTest_wsq.h"
 #include "IceSkatingTest_xjn.h"
 
 
 int main(int argc, char **argv) {
 	std::string str;
-	std::cout << "输入任意键开始测试" << std::endl;
+	std::cout << "按回车键开始测试" << std::endl;
 	getline(std::cin, str);
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
